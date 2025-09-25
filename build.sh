f87fc5ecd69fefa769d5664d1b870545299915e2
syedismailmethappa <syedmethappa0987@gmail.com>
2025-09-26 00:17:58 +0530

    c

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
-# Exit on error
 set -o errexit  
 
-# 1. Build frontend
-cd tudo_frontend && npm install && npm run build
+# 1. Build frontend (Vite -> dist folder)
+cd tudo_frontend
+npm install
+npm run build
 
 # 2. Copy React build files into Django static folder
-cp -r build ../tudo_backend/static/
+cp -r dist ../tudo_backend/static/
 
 # 3. Backend setup
 cd ../tudo_backend
@@ -18,5 +19,4 @@ python manage.py migrate
 
 # 5. Start Gunicorn server
 gunicorn tudo_backend.tudo_backend.wsgi:application --bind 0.0.0.0:8000
-
 
