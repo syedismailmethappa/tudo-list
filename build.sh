bbd3a8fc0159523d75826e5fcd0a32acf1eb42b5
syedismailmethappa <syedmethappa0987@gmail.com>
2025-09-25 23:39:12 +0530

    n

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 
       cd frontend && npm install && npm run build
       cp -r build ../backend/static/
-      cd ../backend && tudo_backend.wsgi:application 
+      cd ../backend 
       pip install -r requirements.txt
       python manage.py collectstatic --noinput
       python manage.py migrate
-     
+      gunicorn tudo_backend.wsgi:application --bind 0.0.0.0:8000
      
