148997ccc604018386c54ed49b7622beb3e1398d
syedismailmethappa <syedmethappa0987@gmail.com>
2025-09-26 22:38:51 +0530

    x

--- tudo_frontend/vite.config.ts
+++ tudo_frontend/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-// https://vite.dev/config/
+// Fix __dirname for ESM
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: path.resolve(__dirname, '../backend/frontend_build'),
+    emptyOutDir: true,
+  }
 })
